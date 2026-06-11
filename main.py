8bd5eac15b31c18ea5f266aee5fde599cfc3fd70
zuluetarafael60-dev <zuluetarafael60@gmail.com>
2026-06-11 18:26:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+import os
 import telebot
 
-# SUSTITUYE EL TOKEN POR EL NUEVO QUE TE DÉ BOTFATHER
-TOKEN ='8689021231:AAEMchNL1ty54Kr8McGSrqbHjE6fCtx8Vmg'
+# Recupera el token desde las variables de entorno de Render
+TOKEN = os.environ.get('TOKEN')
 bot = telebot.TeleBot(TOKEN)
 
 ADMIN_ID = 8747406142
 
 @bot.message_handler(func=lambda message: True)
 def filter_messages(message):
-    if message.from_user.id != ADMIN_ID:
-        if "http" in message.text or "t.me" in message.text:
-            bot.delete_message(message.chat.id, message.message_id)
-            bot.reply_to(message, "⚠️ Enlace detectado y eliminado. Solo el administrador puede enviar enlaces.")
+    try:
+        # Si el mensaje no es del admin y tiene enlaces
+        if message.from_user.id != ADMIN_ID:
+            if "http" in message.text or "t.me" in message.text:
+                bot.delete_message(message.chat.id, message.message_id)
+                # Usamos send_message en lugar de reply_to para evitar errores de referencia
+                bot.send_message(message.chat.id, f"⚠️ @{message.from_user.username}, enlace eliminado. Solo el admin puede enviar enlaces.")
+    except Exception as e:
+        print(f"Error procesando mensaje: {e}")
 
-bot.polling()
+bot.infinity_polling()
